a0650b306f0d622922c812d82c48090569ffde92
temmie0232 <temmie0232@gmail.com>
2025-10-28 18:59:55 +0900

    add: Q1の作成
    - GET /hello で "Hello, World!"と返すAPIを作成
    - @GetMapping

---
+++ src/main/java/com/example/api_practice/ApiPracticeController.java
@@ -0,0 +1,16 @@
+package com.example.api_practice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class ApiPracticeController {
+
+    // /helloアにアクセスするとHello Worldという文字列が返される関数
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
