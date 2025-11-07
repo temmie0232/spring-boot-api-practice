94595ca2244ef2147c06246bf866605fb2e1295a
temmie0232 <temmie0232@gmail.com>
2025-11-07 15:30:32 +0900

    add: Q3の作成 ・URLの一部をパス変数として受け取り、"Hello,{name}!"と返す ・@RequestParam

--- src/main/java/com/example/api_practice/ApiPracticeController.java
+++ src/main/java/com/example/api_practice/ApiPracticeController.java
@@ -8,15 +8,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RestController
 public class ApiPracticeController {
 
-    // /helloアにアクセスするとHello Worldという文字列が返される関数
+    // /helloにアクセスするとHello Worldという文字列が返される関数
     @GetMapping("/hello")
     public String hello() {
         return "Hello, World!";
     }
 
-    // /goodbyeにあくせすすると Good Byeという文字列が返される関数
+    // /goodbyeにアクセスすると Good Byeという文字列が返される関数
     @GetMapping("/goodbye")
     public String goodbe() {
         return "GoodBye!";
     }
+
+    // URLの一部をパス変数として受け取り "Hello, {name}!" と返す関数
+    @GetMapping("/greet/{name}")
+    public String greet(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
 }
